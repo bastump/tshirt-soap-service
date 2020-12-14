2025b320f00220cbd085f92b39066c0c35bbb433
Brian Stump <brianstump@LP1-US-51718881.local>
2020-12-13 23:11:06 -0500

    added small data generator

---
+++ data/populate-inventory.sql
@@ -0,0 +1,5 @@
+INSERT INTO orders (order_id, size, status, email, name, address1, city, state_or_province, postal_code, country) 
+VALUES ('1', '2', 'CREATED', 'fakeemail@gmail.com', 'Brian Stump', '123 Main Street', 'New York', 'NY', '10022', 'USA');
+
+INSERT INTO orders (order_id, size, status, email, name, address1, city, country)
+VALUES ('2', '1', 'CREATED', 'email@email.com', 'Romain C', '123 Main Street', 'Paris', 'France');
